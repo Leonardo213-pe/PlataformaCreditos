bc4933c349104d75c0fa7bc38f1d354dd07f5ae6
Leonardo213-pe <erickfloresaguilar123@gmail.com>
2026-04-25 13:05:28 -0500

    P4: Sesion con ultima solicitud visitada, cache Redis 60s con invalidacion

--- PlataformaCreditosWeb/Program.cs
+++ PlataformaCreditosWeb/Program.cs
@@ -36,6 +36,13 @@ else
     builder.Services.AddDistributedMemoryCache();
 }
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 // Sesiones
 builder.Services.AddSession(options =>
 {
